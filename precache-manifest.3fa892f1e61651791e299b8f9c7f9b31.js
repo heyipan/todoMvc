47c42b38d051a3340452b8f85fc4ba9b393a6600
25042 <qiheng.he@hand-china.com>
2019-07-24 16:57:04 +0800

    first commit

---
+++ precache-manifest.3fa892f1e61651791e299b8f9c7f9b31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba44f4163984359d53e9131e22363b92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99545a02b7284404bcbe",
+    "url": "/static/css/2.13480f0e.chunk.css"
+  },
+  {
+    "revision": "99545a02b7284404bcbe",
+    "url": "/static/js/2.c1d979bd.chunk.js"
+  },
+  {
+    "revision": "c80d4b90e1d4e66b964a",
+    "url": "/static/js/main.ee2ac85f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ precache-manifest.da0b95960f8f41d8a8a22a99574b85bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe8498134ced56b16153e33c6c67897",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2e97f397d63653df082",
+    "url": "/static/css/2.13480f0e.chunk.css"
+  },
+  {
+    "revision": "64153dafd6a831367fc9",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e2e97f397d63653df082",
+    "url": "/static/js/2.ac5e31c2.chunk.js"
+  },
+  {
+    "revision": "64153dafd6a831367fc9",
+    "url": "/static/js/main.0e374341.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
